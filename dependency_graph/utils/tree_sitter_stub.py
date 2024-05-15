b85e27d1bd3bca038965bddde2d09d7920338d2f
友恒 <zhengxunjin.zx@antgroup.com>
2024-05-15 18:22:41 +0800

    add C# stub

--- dependency_graph/utils/tree_sitter_stub.py
+++ dependency_graph/utils/tree_sitter_stub.py
@@ -84,3 +84,83 @@ def generate_java_stub(code: str, include_comments: bool = True) -> str:
         code = "\n".join([c.rstrip() for c in code_bytes_arr.decode().splitlines()])
 
     return code
+
+
+def generate_c_sharp_stub(code: str, include_comments: bool = True) -> str:
+    lib_path = get_builtin_lib_path(TS_LIB_PATH)
+
+    # Initialize the Tree-sitter language
+    language = Language(str(lib_path.absolute()), "c_sharp")
+    parser = Parser()
+    parser.set_language(language)
+
+    code_bytes = code.encode()
+    tree: Tree = parser.parse(code_bytes)
+
+    code_has_changed = False
+    code_bytes_arr = bytearray(code_bytes)
+
+    # Remove using directive
+    query = language.query(
+        dedent(
+            """
+            (using_directive) @using
+            """
+        )
+    )
+    captures = query.captures(tree.root_node)
+    for node, _ in captures:
+        code_has_changed = True
+        code_bytes_arr[node.start_byte : node.end_byte] = SPECIAL_CHAR * (
+            node.end_byte - node.start_byte
+        )
+
+    # Remove method body
+    query = language.query(
+        dedent(
+            """
+            [
+              (method_declaration) @decl
+              (constructor_declaration) @decl
+              (destructor_declaration) @decl
+              (property_declaration) @decl
+              (indexer_declaration) @decl
+            ]
+            """
+        )
+    )
+    captures = query.captures(tree.root_node)
+
+    for node, _ in captures:
+        field_name_to_remove = ("accessors", "body")
+        for field_name in field_name_to_remove:
+            body: Node = node.child_by_field_name(field_name)
+            if body:
+                code_has_changed = True
+                code_bytes_arr[body.start_byte : body.end_byte] = SPECIAL_CHAR * (
+                    body.end_byte - body.start_byte
+                )
+
+    # Remove comment
+    if not include_comments:
+        query = language.query(
+            dedent(
+                """
+                [
+                  (comment) @comment
+                ]
+                """
+            )
+        )
+        captures = query.captures(tree.root_node)
+        for node, _ in captures:
+            code_has_changed = True
+            code_bytes_arr[node.start_byte : node.end_byte] = SPECIAL_CHAR * (
+                node.end_byte - node.start_byte
+            )
+
+    if code_has_changed:
+        # rstrip the trailing whitespaces
+        code = "\n".join([c.rstrip() for c in code_bytes_arr.decode().splitlines()])
+
+    return code
