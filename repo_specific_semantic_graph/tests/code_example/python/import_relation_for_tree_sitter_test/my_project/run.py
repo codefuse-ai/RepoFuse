b2a4169c0b6c3607fb6796a6247bb88b0298bf42
友恒 <zhengxunjin.zx@antgroup.com>
2024-11-11 15:49:16 +0800

    refactor(python_resolver): Optimize import resolution logic

--- repo_specific_semantic_graph/tests/code_example/python/import_relation_for_tree_sitter_test/my_project/run.py
+++ repo_specific_semantic_graph/tests/code_example/python/import_relation_for_tree_sitter_test/my_project/run.py
@@ -1,6 +1,7 @@
 from module_a.foo import foo_function  # Absolute import
 from module_a.submodule.bar import bar_function as bar_alias_func, bar_function_1
 from module_b.baz import baz_function  # Absolute import
+import os # Import not in the repo
 
 print(bar_alias_func())
 print(bar_function_1())
